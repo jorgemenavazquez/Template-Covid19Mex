03d38ccb9a1601325b9afb6ea9c57e0ed2351cd0
ManuelRod-g-tec <63820088+ManuelRod-g-tec@users.noreply.github.com>
2020-05-01 19:24:33 -0500

    Add files via upload

---
+++ assets/js/aviso.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+$('#aviso').hide(0);
+
+
+		$(window).scroll(function(){
+
+
+				var windowHeight = $(window).scrollTop();
+				var contenido2 = $("#contenido2").offset();
+
+				contenido2 = contenido2.top;
+
+
+				if(windowHeight >= contenido2  ){
+					
+
+					$('#aviso').fadeIn(500);
+
+				}else{
+					$('#aviso').fadeOut(500);
+
+				}
+
+
+		});
+
+});
